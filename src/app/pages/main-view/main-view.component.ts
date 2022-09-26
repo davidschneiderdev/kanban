aa5e4cf8ce9e1b63752333a0b63330e053022bdd
David Schneider <david.jay.schneider@gmail.com>
2022-09-26 17:27:57 -0400

    Adding board, columns

--- src/app/pages/main-view/main-view.component.ts
+++ src/app/pages/main-view/main-view.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { Board } from 'src/app/models/board.model';
+import { Column } from 'src/app/models/column.model';
 
 @Component({
   selector: 'app-main-view',
@@ -14,9 +16,31 @@ export class MainViewComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
-
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+  board: Board = new Board('test board', [
+    new Column('Ideas', [
+      "Some random idea",
+      "This is another random idea",
+      "Build an awesome application"
+    ]),
+    new Column('Research', [
+      "Lorem ipsum",
+      "More test",
+      "Photosynthesis"
+    ]),
+    new Column('Todo', [
+      'Get to work', 
+      'Pick up groceries', 
+      'Go home', 
+      'Fall asleep'
+    ]),
+    new Column('Done', [
+      'Get up', 
+      'Brush teeth', 
+      'Take a shower', 
+      'Check e-mail', 
+      'Walk dog'
+    ])
+  ])
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
